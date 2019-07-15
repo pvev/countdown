5e51c6a783629dfdf0b457df66c519357c590ad8
Pablo Velez <pvelez@bp-3.com>
2019-07-14 23:20:52 -0500

    change the config values

--- config/dev.exs
+++ config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
 config :countdown, CountdownWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: 4002],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
@@ -51,8 +51,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :countdown, Countdown.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "pvelez",
+  password: "",
   database: "countdown_dev",
   hostname: "localhost",
   pool_size: 10
--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,8 @@ config :logger, level: :warn
 # Configure your database
 config :countdown, Countdown.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "pvelez",
+  password: "",
   database: "countdown_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
