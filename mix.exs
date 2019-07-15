d7b6578f1a2cf4dd77e67ad1264fb623b5a484e6
Pablo Velez <pvelez@bp-3.com>
2019-07-14 23:59:34 -0500

    Add the auth0 controller along with the user_from_auth_0 model, also add the auth0 dependencies and the ueberauth module

--- mix.exs
+++ mix.exs
@@ -41,7 +41,9 @@ defmodule Countdown.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:plug_cowboy, "~> 1.0"}
+      {:plug_cowboy, "~> 1.0"},
+      {:ueberauth, "~> 0.4"},
+      {:ueberauth_auth0, "~> 0.3"}
     ]
   end
 
