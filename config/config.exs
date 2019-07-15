78b011327f220bab37957c5a02edf915a815200c
Pablo Velez <pvelez@bp-3.com>
2019-07-14 21:05:56 -0500

    first commit

---
+++ config/config.exs
@@ -0,0 +1,27 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# General application configuration
+config :countdown,
+  ecto_repos: [Countdown.Repo]
+
+# Configures the endpoint
+config :countdown, CountdownWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "v4HQFa1vMu5i4YzJ89lVamZr/3n8Eyvr6kh/fFdh2B9yyfINkWrDvmly62ul6cHw",
+  render_errors: [view: CountdownWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Countdown.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:user_id]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
